81330a603e401fa9f75a556e557c98e7a8a71274
Prince Keshri <pince12345keshri@gmail.com>
2023-08-02 12:03:49 +0530

    A

--- setup_server.py
+++ setup_server.py
@@ -1,5 +1,8 @@
 import os
 
-os.system("mkdir tools")
+
 
 os.system("curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall")
+
+
+os.system("mkdir tools")
